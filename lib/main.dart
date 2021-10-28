c05f51d0045dbafdd4eeb9a49da4aeb90fc20af0
Ben Cheong <cheongmengben@outlook.com>
2021-10-28 14:42:57 +0800

    fixed_enabled_button

--- lib/main.dart
+++ lib/main.dart
@@ -3,123 +3,139 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(home: DemoApp()));
 }
+class DemoApp extends StatefulWidget{
+  DemoApp({Key? key}) : super(key: key);
+  @override
+  _DemoAppState createState() => _DemoAppState();
+}
 
-class DemoApp extends StatelessWidget {
+class _DemoAppState extends State<DemoApp> {
   // The build method rebuilds the widget tree if there are any changes.
+  void initState() {
+    super.initState();
+  }
+  
+  bool checkNull = false;
+  bool _btnEnabled = false;
+
+  _doSomething () {
+    ScaffoldMessenger.of(context).showSnackBar(
+                       SnackBar(
+                        content: Text(
+                          'You have signed up successfully.',
+                          style: TextStyle(fontSize: 18),
+                          textAlign: TextAlign.center,
+                          )
+                        ),
+                    );
+  }
+
+  var currentSelectedValue;
+  static const genderTypes = ["Male", "Female", "Other", "Rather not to say"];
+
+  final formKey = GlobalKey<FormState>();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        key: scaffoldKey,
           appBar: AppBar(
             title: Text('Welcome Message'),
           ),
-          body: Center(
+          body: Form(
+              key: formKey,
+              onChanged: () => setState(() => _btnEnabled = formKey.currentState!.validate()),
+          child: Center(
             child: Column(
               children: <Widget>[
-                new Container(
-              child: new Image.asset('images/flutter.png'),
-              alignment: Alignment.center,
-            ),
+                Container(
+                  child: Image.asset('images/flutter.png'),
+                  alignment: Alignment.center,
+                ),
                 Padding(
                 padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                  child:TextField(
+                 child: TextFormField(  
+                  validator: (String? value) {
+                  if(value == null || value.isEmpty){
+                    return "Name cannot be empty";
+                  }
+                  return null;
+                  }, 
                     decoration: const InputDecoration(
                     border: OutlineInputBorder(),
+                    isDense: true,
                     hintText: 'Name'
                     ),
                 ),
                 ),
-                 Padding(
-                 padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                  child:TextField(
+            
+                
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                child:TextFormField(
+                   validator: (value) {
+                  if(value == null || value.isEmpty){
+                    return "Email cannot be empty";
+                  }else if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                    .hasMatch(value)) {
+                      return 'Valid email required';
+                }
+                  return null;
+                },
                     decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: 'Email'
                     ),
                 ),
-                 ),
-                 Padding(
-                 padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                  child:TextField(
+              ),
+
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                child:TextFormField(
+                  validator: (value) {
+                  if(value == null || value.isEmpty){
+                    return "Password cannot be empty";
+                  }
+                  return null;
+                }, 
                     decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: 'Password'
                     ),
                 ),
-                 ),
+              ),
                 
-                MyStatefulWidget(),
- 
-                SnackBarPage()
-                
-                
+              Container(
+                padding: EdgeInsets.all(18),
+                width: 600,
+                child: DropdownButtonFormField<String>(
+                  validator: (value) => value == null ? 'Please select your gender' : null,
+                  isExpanded: true,
+                  hint: Text("Select your gender"),
+                  value: currentSelectedValue,
+                  icon: Icon(Icons.keyboard_arrow_down_rounded),
+                  items: genderTypes.map((String value) {
+                  return DropdownMenuItem(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    currentSelectedValue = newValue;
+                  });
+                },
+              ),
+            ),
+                 ElevatedButton(
+                   onPressed: _btnEnabled? () => _doSomething(): null,
+                   child: Text('Sign up')
+                 ),        
               ],
             ),
           )),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  String? dropdownValue = null;
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      isExpanded: true,
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      iconSize: 2,
-      elevation: 6,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
-      },
-      items: <String>['Male', 'Female', 'Other', 'Rather not to say']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
-  }
-}
-
-
-class SnackBarPage extends StatelessWidget {
-  const SnackBarPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-                  final snackBar = SnackBar(
-            content: const Text('You have signed up successfully'),
-          );
-          
-          // Find the ScaffoldMessenger in the widget tree
-          // and use it to show a SnackBar.
-          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        },
-        child: const Text('Sign up'),
       ),
     );
   }
 }
-
-
